07ae27d514e6e371826d54c8942fc784ee2e6ff2
tuhaisheng <tuhaisheng@soyoung.com>
2019-05-27 22:56:32 +0800

    Initial Commit

---
+++ 水滴效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  水滴效果
+//
+//  Created by tuhaisheng on 2019/5/27.
+//  Copyright © 2019年 tuhaisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 水滴效果/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  水滴效果
+//
+//  Created by tuhaisheng on 2019/5/27.
+//  Copyright © 2019年 tuhaisheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
